ba0051566f689c6deb54385117ac1b39ec8a3746
Prashant Mhatre <mhatrep@gmail.com>
2021-11-11 10:03:38 -0500

    Create ArmstrongNumber.js

---
+++ ArmstrongNumber.js
@@ -0,0 +1,12 @@
+const isArmstrongNumber = function(number){  
+  let numOfDigits = number.length;
+  let sum = 0;  
+  for(let i = 0; i < numOfDigits; i++)
+  {
+    sum += Math.pow(number.charAt(i), numOfDigits);
+   }
+  return sum == number ? true : false;
+}
+console.log(isArmstrongNumber("153"));
+console.log(isArmstrongNumber("1532"));
+console.log(isArmstrongNumber("1634"));
