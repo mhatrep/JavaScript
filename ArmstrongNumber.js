b95f675795644c98b400935a545f86fcee42bc33
Prashant Mhatre <mhatrep@gmail.com>
2021-11-11 10:04:13 -0500

    Update ArmstrongNumber.js

--- ArmstrongNumber.js
+++ ArmstrongNumber.js
@@ -1,8 +1,7 @@
 const isArmstrongNumber = function(number){  
   let numOfDigits = number.length;
   let sum = 0;  
-  for(let i = 0; i < numOfDigits; i++)
-  {
+  for(let i = 0; i < numOfDigits; i++){
     sum += Math.pow(number.charAt(i), numOfDigits);
    }
   return sum == number ? true : false;
